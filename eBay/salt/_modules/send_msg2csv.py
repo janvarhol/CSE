5cb971a2a54b35da4c8a4d7bef4c8a77375e1ec1
amalaguti <amalaguti@saltstack.com>
2019-10-24 15:50:34 -0300

    Update send_msg2csv.py

--- eBay/salt/_modules/send_msg2csv.py
+++ eBay/salt/_modules/send_msg2csv.py
@@ -27,7 +27,7 @@ def save2csv(recipient, subject, mac):
     '''
     Save data to csv file
     '''
-    filename = __salt__['pillar,get']('logdir', '/var/log/salt/stale_minions.csv'
+    filename = __salt__['pillar,get']('logdir', '/var/log/salt/stale_minions.csv')
     with open(filename, mode='a') as email_records:
         email_records_writer = csv.writer(email_records, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         email_records_writer.writerow([datetime.now(), recipient, subject, mac])
