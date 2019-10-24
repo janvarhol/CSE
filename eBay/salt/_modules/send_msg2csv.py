475f7ba57a1060bb36be61ea4076a0ce1e95f272
amalaguti <amalaguti@saltstack.com>
2019-10-24 15:48:07 -0300

    Update send_msg2csv.py

--- eBay/salt/_modules/send_msg2csv.py
+++ eBay/salt/_modules/send_msg2csv.py
@@ -30,7 +30,7 @@ def save2csv(recipient, subject, mac):
     
     with open('/tmp/email_records.csv', mode='a') as email_records:
         email_records_writer = csv.writer(email_records, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        email_records_writer.writerow([datetime.now(), recipient, subject, mac)])
+        email_records_writer.writerow([datetime.now(), recipient, subject, mac])
 
     return True
 
