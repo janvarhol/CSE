7af812201165a58bd15266fa89610daae1f878d9
Malaguti, Adrian <amalaguti@saltstack.com>
2019-11-13 17:16:34 -0300

    .

--- eBay/salt/_runners/run_up_throttle.py
+++ eBay/salt/_runners/run_up_throttle.py
@@ -181,8 +181,8 @@ def execute_luks_check(tgt='*', tgt_type='glob', timeout=None, gather_job_timeou
 
         ret = do_the_job(throttle_lists)
 
-    if show_job_output = True:
-        print(json.dumps(ret, indent=4, sort_keys=True))
+    #if show_job_output = True:
+    #    print(json.dumps(ret, indent=4, sort_keys=True))
 
     return True
 
