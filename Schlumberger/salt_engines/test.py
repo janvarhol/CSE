f029848ad8434afa863150bc9eb6ae7668181ded
Malaguti, Adrian <amalaguti@saltstack.com>
2019-12-05 00:45:16 -0300

    test save events to redis

--- Schlumberger/salt_engines/test.py
+++ Schlumberger/salt_engines/test.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 '''
-A simple test engine, not intended for real use but as an example
+An engine to save Salt events to Redis
 Requires redis
 
+TEST
+1. salt-master -l info
+2. salt-call event.send 'slb/custom/ret' '{status: completed}'
 
 :configuration:
     Example configuration
@@ -35,13 +38,40 @@ log = logging.getLogger(__name__)
 
 
 def redis_set(redis_conn,event):
-    jevent = salt.utils.json.dumps(event)
+    #jevent = salt.utils.json.dumps(event)
 
-    #Filter by event tag
-    if jevent['tag'].startswith('salt/job/'):
+    #Filter by event tag and save as JSON string
+    if event['tag'].startswith('salt/job/'):
         print("PROCESSING SALT JOB EVENT")
-        jevent['event_type'] = 'salt_job'
-        print(jevent)
+        event_record = { "event_type": "salt_job_return", "tag": event['tag'], "more_info": event['data'] }
+        jevent_record = salt.utils.json.dumps(event_record)
+
+        try:
+            redis_conn.set(event['data']['jid'], jevent_record)
+        except:
+            log.error("Engine unable to connect to Redis server")
+
+        print("VERIFYING REDIS DATA")
+        redis_data = redis_conn.get(event['data']['jid'])
+        jredis_data = salt.utils.json.loads(redis_data)
+        print(salt.utils.json.dumps(jredis_data, indent=4))
+
+    elif event['tag'].startswith('slb/custom/'):
+        print("PROCESSING SLB CUSTOM EVENT")
+        event_record = { "event_type": "slb_custom", "tag": event['tag'], "more_info": event['data'] }
+        jevent_record = salt.utils.json.dumps(event_record)
+
+        try:
+            redis_conn.set(event['data']['data']['__pub_jid'], jevent_record)
+        except:
+            log.error("Engine unable to connect to Redis server")
+
+        print("VERIFYING REDIS DATA")
+        redis_data = redis_conn.get(event['data']['data']['__pub_jid'])
+        jredis_data = salt.utils.json.loads(redis_data)
+        print(salt.utils.json.dumps(jredis_data, indent=4))
+    else:
+        log.warning("Engine unable to handle event type")
 
     return True
 
@@ -68,7 +98,7 @@ def start(tags):
     try:
         redis_conn = redis.StrictRedis(host='localhost', port=6379, db=0)
     except:
-        log.debug("Engine unable to connect to Redis server")
+        log.error("Engine unable to connect to Redis server")
 
     while True:
         event = event_bus.get_event(full=True)
