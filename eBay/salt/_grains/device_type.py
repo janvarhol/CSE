51f4fc26ba0e98b349af33545dc1c6bf3b4385f9
amalaguti <amalaguti@saltstack.com>
2019-12-05 15:41:03 -0300

    Create device_type.py

---
+++ eBay/salt/_grains/device_type.py
@@ -0,0 +1,29 @@
+import salt.modules.cmdmod
+ 
+__salt__ = {
+    'cmd.run': salt.modules.cmdmod._run_quiet,
+    'cmd.retcode': salt.modules.cmdmod._retcode_quiet,
+    'cmd.run_all': salt.modules.cmdmod._run_all_quiet,
+}
+DESKTOPS = ['Other', 'Desktop', 'Space-saving', 'Tower']
+LAPTOPS = = ['Laptop', 'Notebook']
+SERVER = = ['Server']
+`
+def devicetype():
+    grains = {}
+    try:
+        result = __salt__['cmd.run_all']('dmidecode --string chassis-type')
+        if result['retcode'] == 0:
+            if result['stdout'] in DESKTOPS:
+                grains['device_type'] = 'Desktop'
+            elif result['stdout'] in LAPTOPS:
+                grains['device_type'] = 'Laptop'
+            elif result['stdout'] in SERVER:
+                grains['device_type'] = 'Server'
+            else
+                grains['device_type'] = 'Unknown'
+        else:
+            grains['device_type'] = 'ERROR'
+    except Exception as e:
+        log.error("Custom grain device_type error: " + e)
+    return grains
