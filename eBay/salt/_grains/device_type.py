657ba0b84cd57f045e39e4a18a74aaaf1e6e306f
amalaguti <amalaguti@saltstack.com>
2019-12-09 16:45:23 -0300

    Update device_type.py

--- eBay/salt/_grains/device_type.py
+++ eBay/salt/_grains/device_type.py
@@ -17,7 +17,7 @@ SERVER = ['Server']
 def device_type():
     grains = {}
     
-    if __grains['osfinger'] == 'Debian-9':
+    if __grains__['osfinger'] == 'Debian-9':
         log.info("IT IS A DEBIAN SYSTEM")
         grains['device_type'] = 'Debian System'
         return grains
