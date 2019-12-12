682335e8943b702fb06a0ee0336dd2fd0c6090ff
Malaguti, Adrian <amalaguti@saltstack.com>
2019-12-12 12:23:57 -0300

    delete Sch

--- Schlumberger/confs/scripts_misc/get_engines.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-wget -O /srv/salt_engines/test.py https://raw.githubusercontent.com/amalaguti/CSE/master/Schlumberger/salt_engines/test.py
--- Schlumberger/pillar/basic.sls
+++
@@ -1,2 +0,0 @@
-CSE: Schlumberger-git
-PS: Adrian Malaguti-git
