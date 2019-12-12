82b71a83031864b30d20543bef3cf9b6cfb12115
amalaguti <amalaguti@saltstack.com>
2019-12-12 09:53:37 -0300

    Custom grain based on url post call

---
+++ Schlumberger/salt/url_post.py
@@ -0,0 +1,34 @@
+import salt.modules.cmdmod
+import logging
+import os
+
+
+import requests
+import json
+
+# Disable insecure warning message
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+url = 'https://httpbin.org/post'
+headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+payload = {'key1': 'value1', 'key2': 'value2'}
+
+
+
+log = logging.getLogger(__name__)
+
+def url_post():
+    grains = {}
+      
+    try:
+        r = requests.post(url, json=payload, headers=headers, verify=False)
+        log.info("url_post call status code: " + r.status_code)
+        if r.status_code == 200:
+            log.info("JSON response: " + json.dumps(r.json()['json'], indent=4))
+            grains['url_post'] = r.json()['json']
+        else:
+            grains['device_type'] = 'ERROR WITH POST CALL'
+    except Exception as e:
+        log.error("Custom grain url_post error: " + e)
+    return grains
