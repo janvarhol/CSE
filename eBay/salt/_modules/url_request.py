3270e48671034d26ea7bb07d9710016477c6e7f2
amalaguti <amalaguti@saltstack.com>
2020-01-07 13:08:17 -0300

    Update url_request.py

--- eBay/salt/_modules/url_request.py
+++ eBay/salt/_modules/url_request.py
@@ -20,6 +20,10 @@ payload_post = {'key1': 'value1', 'key2': 'value2'}
 # GET DATA
 url_get = 'https://httpbin.org/get'
 
+# PUT DATA
+url_put = 'https://httpbin.org/put'
+payload_put = {'include_scan': True }
+
 
 log = logging.getLogger(__name__)
 
@@ -64,6 +68,46 @@ def request_get():
             #print("ERROR NOT 200")
             return {}
 
+def request_put():
+    try:
+        res = requests.put(url_put, json=payload_put, verify=False)
+        log.info("url_post call status code: " + str(res.status_code))
+
+        # REQUEST note:
+        # if response: will return True for anything between 200 to 400, False otherwise
+
+        # REQUEST note:
+        # requests.raise_for_status() raises an HTTPError for certain status codes
+        # If the status code indicate a successful request, the exception is not raised
+        res.raise_for_status()
+
+        # request status code
+        # print(res.status_code)
+    except HTTPError as http_err:
+        #print('HTTP error occurred: ' + str(http_err))
+        log.error('url_request module: HTTP error occurred: ' + str(http_err))
+        #raise SaltInvocationError(str('http_err'))
+        return {}
+    except Exception as err:
+        #print('Other error occurred: ' + str(err))
+        log.error('url_request module: Other error occurred: ' + str(err))
+        #raise SaltInvocationError(str(err))
+        return {}
+    else: # everything is fine, proceed
+        if res.status_code == 200:
+            log.info('url_request module: 200 query OK')
+            #print("JSON response: " + json.dumps(res.json(), indent=4))
+            
+            # Some checking before returning data
+            # Check data is dictionary and expected key 'origin' in data
+            if type(res.json()) is dict and 'origin' in res.json():
+                return res.json()
+            else:
+                return {}
+        else:
+            log.error('url_request module: Invalid response - not 200 code')
+            #print("ERROR NOT 200")
+            return {}
 
 def request_post():
     try:
