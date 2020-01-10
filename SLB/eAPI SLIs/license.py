251b79d44b3da290c67db074accbc831d3ca667a
amalaguti <amalaguti@saltstack.com>
2020-01-10 09:35:40 -0300

    Update license.py

--- SLB/eAPI SLIs/license.py
+++ SLB/eAPI SLIs/license.py
@@ -1,7 +1,7 @@
 import json
 
 from sseapiclient.tornado import SyncClient
-client = SyncClient.connect('https://localhost', 'root', 'saltstack!2019', ssl_validate_cert=False)
+client = SyncClient.connect('https://localhost', 'root', 'xxxxx', ssl_validate_cert=False)
 print(json.dumps(client.api.license.check_usage(), indent=4))
 print(json.dumps(client.api.license.get_current_license(), indent=4))
 
