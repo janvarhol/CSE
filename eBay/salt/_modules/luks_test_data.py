e0422ee5c790525e4eeee5888c4886cdd2cd9262
amalaguti <amalaguti@saltstack.com>
2020-01-30 17:06:07 -0300

    Update luks_test_data.py

--- eBay/salt/_modules/luks_test_data.py
+++ eBay/salt/_modules/luks_test_data.py
@@ -8,10 +8,170 @@ import logging
 import json
 from datetime import datetime
 
-#import time
-
 log = logging.getLogger(__name__)
 
+
+def is_disk_encrypted(block_device, block_devices, TYPE):
+    '''
+    Run cryptsetup isLuks /dev/<device>
+    retcode 0 = Disk Encrypted
+    retcode 1 = Disk not encrypted
+    '''
+    '''
+
+    # IMPORTANT: REPLACE device by block_device !!!
+
+    print("--->>> Checking disk encrypted on device: " + device)
+    log.info("--->>> Checking disk encrypted on device: " + device)
+    # Check if device name ends with partition number, like /dev/sda5
+    if device[-1:].isdigit():
+        # BEING EXTRA CAREFUL, REDUNDANT, WIH STR AND UNICODE STRINGS
+        # IF THIS FIXES THE ISSUE, LATER POLISH THE CODE
+        cryptsetup_bin = __salt__['cmd.which']('cryptsetup')
+        cryptsetup_bin = str(cryptsetup_bin)
+        log.info("cryptsetup_bin: " + str(cryptsetup_bin))
+        log.info(type(cryptsetup_bin))
+        if cryptsetup_bin != None and len(cryptsetup_bin) > 9:
+            log.info("--->> cryptsetup_bin: " + str(cryptsetup_bin))
+            cryptsetup_isLuks_cmd = cryptsetup_bin + ' isLuks ' + str(device)
+            log.info("Running cryptsetup command: " + cryptsetup_isLuks_cmd)
+            cryptsetup_isLuks = __salt__['cmd.retcode'](cryptsetup_isLuks_cmd, ignore_retcode=True)
+        else:
+            log.warning("cryptsetup binary was not found in path!!!")
+            return 1
+
+    elif device[-1:].isalpha():
+    # if device name ends with alpha, meaning it's a disk, like /dev/sdb
+        print("--->>> Scanning disk for LVM information")
+        log.info("--->>> Scanning disk for LVM information")
+        try:
+            lvm_pv_info = __salt__['lvm.pvdisplay'](device)
+            lvm_vol_group_name = lvm_pv_info[device]['Volume Group Name']
+            lvm_lv_info = __salt__['lvm.lvdisplay'](lvm_vol_group_name)
+            for log_vol_name in lvm_lv_info:
+              lvm_log_vol_name = lvm_lv_info[log_vol_name]['Logical Volume Name']
+              print("--->>> Scanned Logical Volume Name: " + lvm_log_vol_name)
+              log.info("--->>> Scanned Logical Volume Name: " + lvm_log_vol_name)
+
+              # Run crypsetup using LVM Logical Volume Name instead of device name
+              # /dev/sdb vs /dev/home/homevol
+              cryptsetup_isLuks = __salt__['cmd.retcode']('cryptsetup isLuks ' + lvm_log_vol_name, ignore_retcode=True)
+              # if cryptsetup is not 0, return failure, disk not encrypted
+              if cryptsetup_isLuks >= 1:
+                print("--->>> cryptsetup on scanned Logical Volume Name FAILED: " + lvm_log_vol_name)
+                log.warning("--->>> cryptsetup on scanned Logical Volume Name FAILED: " + lvm_log_vol_name)
+                return cryptsetup_isLuks
+        except:
+            # If something goes wrong, return 1 (False / Disk not encrypted, must be fixed)
+            return 1
+    else:
+        print("--->>> Un-handled case, returning Disk not encrypted")
+        return 1
+#    print(cryptsetup_isLuks)
+
+    return cryptsetup_isLuks
+    '''
+
+    # Testing fix for linux_raid_member
+    if block_devices[block_device][TYPE] == 'linux_raid_member':
+        # assuming it is encrypted
+        print("--->>> linux_raid_member device: " + block_device)
+        log.info("--->>> linux_raid_member device: " + block_device)
+
+        return 0
+
+    return 1
+
+def is_boot_partition(block_device, block_devices, mount_points, partitions):
+    '''
+    Check if a non encrypted device is the boot partition
+    True = the non encrypted device is the boot partition
+    False = the non encrypted device IS NOT the boot partition
+    '''
+
+    # Read mount points from /etc/fstab
+    # mount_points = __salt__['mount.fstab']()
+    # partitions = mount_points.keys()
+    # Note: in testing function mount_points and partition
+    # are passed from main
+
+    # TROUBLESHOOTING INFO
+    #print("__is_boot_partition_MOUNT_POINTS: " + json.dumps(mount_points, indent=4))
+    #print("__is_boot_partition_PARTITIONS: " + str(partitions))
+    #print("__is_boot_partition_BLOCK_DEVICE: " + block_device)
+    #print("__is_boot_partition_BLOCK_DEVICES: " + json.dumps(block_devices, indent=4))
+    # END TROUBLESHOOTING INFO
+
+    IS_BOOT = False
+    UUID = 'UUID'
+
+    if "UUID" in block_devices[block_device].keys():
+        UUID = 'UUID'
+        print("Using UUID")
+        block_device_UUID = block_devices[block_device][UUID]
+        print("block_device_UUID: " + block_device_UUID)
+    elif "PTUUID" in block_devices[block_device].keys():
+        UUID = 'PTUUID'
+        print("Using PTUUID")
+        block_device_UUID = block_devices[block_device][UUID]
+        print("block_device_UUID: " + block_device_UUID)
+    elif "PARTUUID" in block_devices[block_device].keys():
+        UUID = 'PARTUUID'
+        print("Using PARTUUID")
+        block_device_UUID = block_devices[block_device][UUID]
+        print("block_device_UUID: " + block_device_UUID)
+
+    #block_device_UUID = __salt__['disk.blkid'](block_device)[block_device][UUID]
+    print("--- BLOCK DEVICE UUID --- : " + block_device_UUID)
+    print("")
+    print("")
+
+    # /boot partition UUID match check (boot partitions can be not encrypted)
+    if '/boot' in partitions:
+        print("Checking /boot partition")
+        log.info("Checking /boot partition")
+
+        # Get and compare UUIDs from block partition and block device
+        #--block_device_UUID = __salt__['disk.blkid'](block_device)[block_device][UUID]
+        block_device_UUID = block_devices[block_device][UUID]
+        boot_UUID = mount_points['/boot']['device'].replace('UUID=', '')
+        print("Block device " + block_device + " UUID: " + block_device_UUID)
+        log.info("Block device " + block_device + " UUID: " + block_device_UUID)
+        print("Boot /boot partition UUID: " + boot_UUID)
+        log.info("Boot /boot partition UUID: " + boot_UUID)
+
+        if boot_UUID == block_device_UUID:
+            print("UUID MATCH for /boot partition and " + block_device + " device")
+            log.info("UUID MATCH for /boot partition and " + block_device + " device")
+            #return True
+            IS_BOOT = True
+
+    # /boot/efi partition UUID match check (boot partitions can be not encrypted)
+    if '/boot/efi' in partitions:
+        print("Checking /boot/efi partition")
+        log.info("Checking /boot/efi partition")
+
+        # Get and compare UUIDs from block partition and block device
+        #--block_device_UUID = __salt__['disk.blkid'](block_device)[block_device][UUID]
+        block_device_UUID = block_devices[block_device][UUID]
+        boot_UUID = mount_points['/boot/efi']['device'].replace('UUID=', '')
+        print("Block device " + block_device + " UUID: " + block_device_UUID)
+        log.info("Block device " + block_device + " UUID: " + block_device_UUID)
+        print("Boot /boot/efi partition UUID: " + boot_UUID)
+        log.info("Boot /boot/efi partition UUID: " + boot_UUID)
+
+        if boot_UUID == block_device_UUID:
+            print("UUID MATCH for /boot/efi partition and " + block_device + " device")
+            log.info("UUID MATCH for /boot/efi partition and " + block_device + " device")
+            #return True
+            IS_BOOT = True
+
+    if IS_BOOT:
+        return True
+    else:
+        return False
+
+        
 def test_data():
     skip_osfinger_list = ['Raspbian-9', 'Raspbian-10']
 
