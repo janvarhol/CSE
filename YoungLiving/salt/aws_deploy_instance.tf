8e4d8c48be0637d54f9fd5b50c4f4d1edf21bd54
amalaguti <amalaguti@saltstack.com>
2020-01-31 18:53:50 -0300

    Create aws_deploy_instance.tf

---
+++ YoungLiving/salt/aws_deploy_instance.tf
@@ -0,0 +1,41 @@
+# Sample Terraform template
+/* This terraform template deploys:
+     - Single instance
+     - aws provider
+*/
+
+provider "aws" {
+  access_key = "REDACTED"
+  secret_key = "REDACTED"
+  region     = "us-east-2"
+}
+
+resource "aws_instance" "tf-insta1" {
+  ami           = "REDACTED"
+  instance_type = "t2.micro"
+  security_groups = ["default"]
+
+
+  connection {
+      type = "ssh"
+      user = "root"
+      password = "REDACTED"
+      timeout = "2m"
+      host = self.private_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "curl -L https://bootstrap.saltstack.com -o install_salt.sh",
+      "sh install_salt.sh -P -A 172.31.45.90 ",
+      "cat <<EOF > /etc/salt/minion.d/autosign_grains.conf",
+      "autosign_grains:",
+      "  - terraform_id",
+      "EOF",
+      "salt-call --local grains.set terraform_id 'tf_12345'",
+      "systemctl stop salt-minion",
+      "sleep 10; systemctl start salt-minion",
+      "systemctl enable salt-minion"
+     ]
+   }
+}
