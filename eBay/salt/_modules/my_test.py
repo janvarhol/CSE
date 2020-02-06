f54b9807a3d33f4384618aa985ffd88986407372
amalaguti <amalaguti@saltstack.com>
2020-02-06 14:34:36 -0300

    Update my_test.py

--- eBay/salt/_modules/my_test.py
+++ eBay/salt/_modules/my_test.py
@@ -1,10 +1,20 @@
 import logging
 import json
-from datetime import datetime, timedelta
 
 log = logging.getLogger(__name__)
 
-def get_keys(device=None):
+def get_crypt_label(device):
+    #lsblk -o NAME,TYPE /dev/sda5 | awk '$2 == "crypt" {print $1}'
+    cmd = 'lsblk -o NAME,TYPE ' + device + ' | awk \'$2 == "crypt" {print $1}\''
+    crypt_label = __salt__['cmd.run_all'](cmd, python_shell=True)
+
+    # Removing first two characters from  `-sda5_crypt
+    crypt_label = crypt_label['stdout'][2:]
+    log.info('--->>> get_crypt_label: ' + crypt_label)
+
+    return crypt_label
+
+def get_keys_v2(device=None):
     """
     Returns the decryption keys for this
 
@@ -14,14 +24,19 @@ def get_keys(device=None):
     """
 
     if device is not None:
+        # AM: get crypt label
+        device_crypt_label = get_crypt_label(device)
         # format(volume=self.label) #, "]
         # AM: add function to replace label
         # lsblk -o NAME,TYPE,MOUNTPOINT /dev/sda5 | grep crypt --> parse string
+        # it has to be like:
+
         #cmd = 'dmsetup table {self.label} --showkeys'
-        cmd = 'dmsetup table sda5_crypt --showkeys'
+        cmd = 'dmsetup table ' + device_crypt_label + ' --showkeys'
     else:
         cmd = "dmsetup table --showkeys "
 
+    log.info('--->>> running cmd: ' + cmd)
     data = __salt__['cmd.run_all'](cmd, python_shell=True)
 
     keys = {}
