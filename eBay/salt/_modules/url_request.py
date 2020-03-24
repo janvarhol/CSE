c5914151e3cd3dd62dba865147abc042d16d332b
amalaguti <amalaguti@saltstack.com>
2020-03-24 17:31:45 -0300

    added get_minion_data()

--- eBay/salt/_modules/url_request.py
+++ eBay/salt/_modules/url_request.py
@@ -145,6 +145,48 @@ def request_post():
             return {}
 
 
+        
+def get_minion_data():
+    try:
+        res = requests.get(url_get, verify=False)
+        log.info("url_post call status code: " + str(res.status_code))
+
+        # REQUEST note:
+        # if response: will return True for anything between 200 to 400, False otherwise
+
+        # REQUEST note:
+        # requests.raise_for_status() raises an HTTPError for certain status codes
+        # If the status code indicate a successful request, the exception is not raised
+        res.raise_for_status()
+
+        # request status code
+        # print(res.status_code)
+    except HTTPError as http_err:
+        #print('HTTP error occurred: ' + str(http_err))
+        log.error('url_request module: HTTP error occurred: ' + str(http_err))
+        #raise SaltInvocationError(str('http_err'))
+        return {}
+    except Exception as err:
+        #print('Other error occurred: ' + str(err))
+        log.error('url_request module: Other error occurred: ' + str(err))
+        #raise SaltInvocationError(str(err))
+        return {}
+    else: # everything is fine, proceed
+        if res.status_code == 200:
+            log.info('url_request module: 200 query OK')
+            #print("JSON response: " + json.dumps(res.json(), indent=4))
+            
+            # Some checking before returning data
+            # Check data is dictionary and expected key 'origin' in data
+            if type(res.json()) is dict and 'origin' in res.json():
+                return res.json()
+            else:
+                return {}
+        else:
+            log.error('url_request module: Invalid response - not 200 code')
+            #print("ERROR NOT 200")
+            return {}        
+        
 # post
 #print(json.dumps(request_post(), indent=4, sort_keys=True))
 
