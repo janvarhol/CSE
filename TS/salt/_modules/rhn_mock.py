f0816ac8f8f27ce65526517a5b2122ae6cb9a4cb
Adrian Malaguti <amalaguti@saltstack.com>
2020-04-12 18:15:47 -0300

    logging stdout

--- TS/salt/_modules/rhn_mock.py
+++ TS/salt/_modules/rhn_mock.py
@@ -12,7 +12,9 @@ log = logging.getLogger(__name__)
 def hammer_info(minion_id=None):
     if minion_id is not None:
         rhn_info = __salt__['cmd.run_all']('/srv/salt/RHN_Satellite_Hammer/hammer.sh ' + minion_id)
-        log.info(rhn_info)
+        if rhn_info['retcode'] == 0:
+            output = rhn_info['stdout']
+            log.info("RHN MOCK: hammer_info: %s": json.dumps(output, indent=4))
     else:
         log.error("RHN MOCK: hammer_info: No minion id received")
         return False
