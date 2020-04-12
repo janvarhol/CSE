e19781c2677bc38c44cd7a18ac4ac4d4fc15457e
Adrian Malaguti <amalaguti@saltstack.com>
2020-04-12 19:12:35 -0300

    mock done

--- TS/salt/_modules/rhn_mock.py
+++ TS/salt/_modules/rhn_mock.py
@@ -11,10 +11,31 @@ log = logging.getLogger(__name__)
 
 def hammer_info(minion_id=None):
     if minion_id is not None:
+
+        # Execute hammer command with Salt dunder
         rhn_info = __salt__['cmd.run_all']('/srv/salt/RHN_Satellite_Hammer/hammer.sh ' + minion_id)
+
+        # Check for return code
         if rhn_info['retcode'] == 0:
-            output = rhn_info['stdout']
-            log.info("RHN MOCK: hammer_info: %s": json.dumps(output, indent=4))
+            rhn_info_output = rhn_info['stdout']
+
+            # Tweak output
+            # Removing first line from string
+            rhn_info_tweaked = rhn_info_output.split('\n', 1)[1:][0]
+            # Loading data as JSON
+            rhn_info_tweaked = json.loads(rhn_info_tweaked)
+
+            log.info("RHN MOCK: hammer_info: %s" % rhn_info_tweaked)
+
+            # Return required info
+            # TODO: Verify the output when minion is not registered / not exist
+            # EXAMPLE!!!!
+            if 'Registered To' in rhn_info_tweaked['Subscription Information'].keys():
+                if rhn_info_tweaked['Subscription Information']['Registered To'] is not False and rhn_info_tweaked['Subscription Information']['Registered To'] != "":
+                    return True
+
+
+            return False
     else:
         log.error("RHN MOCK: hammer_info: No minion id received")
         return False
