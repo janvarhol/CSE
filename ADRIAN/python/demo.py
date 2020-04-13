00fd2bdd02f1a152d37edab2921aecf6c882caa0
Adrian Malaguti <amalaguti@saltstack.com>
2020-04-13 18:22:53 -0300

    python unittest cases examples

---
+++ ADRIAN/python/demo.py
@@ -0,0 +1,18 @@
+contact = {}
+
+def add_contact(name):
+    contact['person'] = { name : {} }
+    return contact
+
+def get_contact(name):
+    return contact['person'][name]
+
+def maintenance_check():
+    return True
+
+'''
+name = 'Adrian'
+contact = add_contact(name)
+#print(contact)
+print(get_contact('Adrian'))
+'''
---
+++ ADRIAN/python/test_demo.py
@@ -0,0 +1,40 @@
+#from unittest import TestCase
+import unittest
+import demo
+
+class demoTest(unittest.TestCase):
+    #def setUp(self) -> None:  #python3 syntax only
+    def setUp(self):
+        name = 'Adrian'
+        self.contact = demo.add_contact(name)
+
+    #def tearDown(self) -> None:  #python3 syntax only
+    def tearDown(self):
+        pass
+
+
+    def test_add_contact(self):
+        '''
+        Test proper addition of a new contact
+        '''
+        #self.fail()
+        self.assertEqual({'person': {'Adrian': {}}}, self.contact)
+
+    def test_get_missing_contact(self):
+        '''
+        Test if KeyError is raised when get_contact() is called for a missing person
+        '''
+        with self.assertRaises(KeyError):
+            demo.get_contact('FakePerson')
+
+    def test_maintenance_check(self):
+        '''
+        Test if KeyError is raised when get_contact() is called for a missing person
+        '''
+        self.assertTrue(demo.maintenance_check())
+
+    # Work in progress, function not ready
+    # Skip this test meanwhile
+    @unittest.skip("Work in Progress")
+    def test_TBD(self):
+        self.assertFalse(demo.TBD())
